c1039f837c00b7cb864121f09bf66dfd3d9c794c
Christian Schläppi <christian.schlaeppi@codedump.ch>
2025-01-01 16:24:01 +0100

    initial commit

---
+++ pinger/main.go
@@ -0,0 +1,147 @@
+package pinger
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"net"
+	"os"
+	"runtime"
+	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
+)
+
+type ip_version int
+
+func DummyPing(addr string) (string, error) {
+	if time.Now().Unix() % 15 == 0 {
+		return "", errors.New(fmt.Sprintf("failed to get reply from %v", addr))
+	}
+
+	return fmt.Sprintf("received reply from %+s", addr),  nil
+}
+
+func lookupAddress(s string) ([]net.IP, error) {
+	result, err := net.LookupIP(s)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func determineAddressFamily(s string) (ip_version) {
+	addr := net.ParseIP(s)
+
+	switch {
+		case net.IP(addr).To4() != nil:
+			return 4
+		case net.IP(addr).To16() != nil:
+			return 6
+		default:
+			return -1
+	}
+}
+
+func Ping(addr string) (string, error) {
+	switch runtime.GOOS {
+		case "darwin", "ios":
+		case "linux":
+			slog.Debug("you may need to adjust the net.ipv4.ping_group_range kernel state")
+		default:
+			return "", errors.New("Unsupported Operating system")
+	}
+
+	var network string
+	var l_addr string
+	var recv_proto int
+	var icmp_type icmp.Type
+
+	switch {
+		case determineAddressFamily(addr) == 4:
+			network, l_addr, recv_proto, icmp_type = "udp4", "0.0.0.0", 1, ipv4.ICMPTypeEcho
+		case determineAddressFamily(addr) == 6:
+			network, l_addr, recv_proto, icmp_type = "udp6", "::", 58, ipv6.ICMPTypeEchoRequest
+		default:
+			// most likely a hostname.. let's first try to look it up and then figure out the rest
+			addr_slice, err := lookupAddress(addr)
+			new_addr := addr_slice[0].String()
+			if err != err {
+				return "", errors.New(fmt.Sprintf("error while looking up %v: %v", addr, err))
+			}
+
+			if determineAddressFamily(new_addr) == 4 {
+				network, l_addr, recv_proto, icmp_type = "udp4", "0.0.0.0", 1, ipv4.ICMPTypeEcho
+			} else if determineAddressFamily(new_addr) == 6 {
+				network, l_addr, recv_proto, icmp_type = "udp6", "::", 58, ipv6.ICMPTypeEchoRequest
+			}
+
+			// Keeping it here just in case
+			slog.Debug(
+				fmt.Sprintf("default mapping performend on %v", addr),
+				slog.String("network", network),
+				slog.String("l_addr", l_addr),
+				slog.Int("recv_proto", recv_proto),
+				slog.String("icmp_type", fmt.Sprintf("%d", icmp_type)),
+				slog.String("name lookup", fmt.Sprintf("%v", addr_slice)),
+				slog.String("new addr", new_addr),
+			)
+
+			addr = new_addr
+	}
+
+	conn, err := icmp.ListenPacket(network, l_addr)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("icmp.ListenPacket: %+v", err))
+	}
+	defer conn.Close()
+
+	err = conn.SetDeadline(time.Now().Add(time.Millisecond*500))
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("Setting connection deadline: %+v", err))
+	}
+
+	message := icmp.Message{
+		Type: icmp_type , Code: 0,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff, Seq: 1,
+			Data: []byte("ICMP echo request"),
+		},
+	}
+
+	wb, err := message.Marshal(nil)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("Marshalling Message: %+v", err))
+	}
+	
+	if _, err := conn.WriteTo(wb, &net.UDPAddr{IP: net.ParseIP(addr), Zone: ""}); err != nil {
+		return "", errors.New(fmt.Sprintf("Writing to conn: %+v", err))
+	}
+
+	rb := make([]byte, 1500)
+	n, peer, err := conn.ReadFrom(rb)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("Reading from conn: %+v", err))
+	}
+
+	// icmpv4 proto number -> 0x01 -> 1
+	// icmpv6 proto number -> 0x3a -> 58
+	//rm, err := icmp.ParseMessage(58, rb[:n])
+	rm, err := icmp.ParseMessage(recv_proto, rb[:n])
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("parsing message: %+v", err))
+	}
+
+	switch rm.Type {
+		case ipv4.ICMPTypeEchoReply:
+			return fmt.Sprintf("Reply %v - Type: %d - Checksum: %d - Raw: %v ", peer, rm.Type, rm.Checksum, rm), nil
+		case ipv6.ICMPTypeEchoReply:
+			return fmt.Sprintf("Reply %v - Type: %d - Checksum: %d - Raw: %v ", peer, rm.Type, rm.Checksum, rm), nil
+		default:
+			return fmt.Sprintf("Non-Reply: %v - Type: %d - Checksum: %d - Raw: %v", peer, rm.Type, rm.Checksum, rm), nil
+	}
+}
+
---
+++ pinger/ping_test.go
@@ -0,0 +1,79 @@
+package pinger
+
+import (
+	"testing"
+	"fmt"
+	"net"
+)
+
+type input struct {
+	addr string
+	exp ip_version
+}
+
+var addresses = []input{
+	input{"1.1.1.1", 4},
+	input{"192.168.1.1", 4},
+	input{"8.8.8.8", 4},
+	input{"127.0.0.1", 4},
+	input{"::1", 6},
+	input{"2a00:1450:400a:802::200e", 6},
+}
+
+func TestDetermineAddressFamily(t *testing.T) {
+	for _, test := range addresses {
+		result:= determineAddressFamily(test.addr)
+		if result == -1 {
+			t.Errorf("input wrongly determined '%v' not %v", result, test.exp)
+		}
+
+		if result != test.exp {
+			t.Errorf("input determined as '%v' not %v", result, test.exp)
+		}
+	}
+}
+
+var ping_addresses = []input{
+	input{"2a00:1450:400a:802::200e", 6},
+	input{"185.178.192.107", 4},
+	input{"nevious.ch", 4},
+	input{"google.com", 6},
+}
+
+func TestPingDestination(t *testing.T) {
+	for _, test := range ping_addresses {
+		result, err := Ping(test.addr)
+		if err != nil {
+			t.Errorf("threw error, unexpected: %+v", err)
+		}
+
+		fmt.Printf("Pinging result: %v\n", result)
+	}
+}
+
+type string_input struct {
+	addr string
+	exp net.IP
+}
+
+var lookup_addresses = []string_input {
+	string_input{"example.com", net.ParseIP("2606:2800:21f:cb07:6820:80da:af6b:8b2c")},
+	string_input{"nevious.ch", net.ParseIP("75.2.60.5")},
+}
+
+func TestLookupAddress(t *testing.T) {
+	for _, test := range lookup_addresses {
+		result, err := lookupAddress(test.addr)
+		if err != nil {
+			t.Errorf("threw error, unexpected: %+v\n", err)
+		}
+
+		for _, element := range result {
+			if element.String() == test.exp.String() {
+				t.Logf("%v: %v Found in %v", test.addr, test.exp, result)
+			}
+		}
+
+	}
+}
+
